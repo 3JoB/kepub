12a4b48dea8bc86bab12a6899c3825f58a26d4ae
3JoB <54469706+3JoB@users.noreply.github.com>
2022-11-04 22:34:26 +0800

    Update build.sh

--- script/build.sh
+++ script/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  curl -fsSL https://github.com/KaiserLancelot/klib/releases/download/v1.25.2/klib-1.25.2-Linux.deb \
+  curl -fsSL https://github.com/3JoB/klib/releases/download/v1.25.2-Patch/klib-1.25.2-Linux.deb \
     -o klib.deb
   sudo dpkg -i klib.deb
 else
